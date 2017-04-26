ef3415cfe59fbb1539d95e216a4776b2a6f9298a
JanouCrastes <janou.crastes@gmail.com>
2017-04-26 15:33:00 +0200

    correct admin

--- Sysconf/ConnexionAdmin.php
+++ Sysconf/ConnexionAdmin.php
@@ -6,7 +6,7 @@ require("config.php");
 if(isset($_POST)){
 $mysqli = new mysqli(config_local::SERVERNAME,config_local::USER,config_local::PASSWORD,config_local::DBNAME);
 $user = $_POST['user'];
-$password = $_POST['pass']
+$password = $_POST['pass'];
 
 $req = $mysqli->prepare('SELECT id, nom FROM administrateur WHERE login =? && password =?');
 $req->bind_param('si', $user, $password);
